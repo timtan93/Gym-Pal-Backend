ed4ec332badec47d8661c430be551cb21adaea3a
github username <github email address>
2019-03-06 11:12:46 +0000

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,67 @@
 class UsersController < ApplicationController
     def index
-        @users = User.all
-        render json: @users
-      end
-    
-      def show
-        @user = User.find(params[:id])
-        render json: @user
-      end
+      @users = User.all
+      render json: @users
+    end
 
-      def create 
-        @user = User.new(name: params[:name], email: params[:email], password: params[:password])
-        if @user.save 
-          render json: @user 
-        else 
-          render json: {error: 'Unable to create user.'}, status: 400
-        end 
-      end
+    def signin 
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        render json: {name: @user.name, token: issue_token({id: @user.id})}
+      else 
+        render json: {error: 'Username/password combination invalid.'}, status: 401
+      end 
+    end 
 
-      def destroy
-        @user = User.find_by(id: params[:id])
-        if @user
-          @user.destroy
-          render json: {message: 'User deleted.'}
-        else
-          render json: {error: 'User not found.'}, status: 404
-        end
-      end
+    def validate 
+      @user = get_current_user
+      if @user 
+        render json: {name: @user.name, token: issue_token({id: @user.id})}
+      else
+        render json: {error: 'Username/password combination invalid.'}, status: 401
+      end 
+    end 
 
-      def update
-        @user = User.find_by(id: params[:id])
-        if @user
-          @user.update(name: params[:name])
-          render  json: @user
-        else 
-          render json: {error: 'User not found.'}, status: 404
-        end
+    def get_workout
+      @user = get_current_user
+      if @user
+        render json: @user.workouts
+      else
+        render json: {error: 'Not a valid user.'}, status: 401
+      end 
+    end 
+  
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+
+    def create 
+      @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+      if @user.save 
+        render json: @user 
+      else 
+        render json: {error: 'Unable to create user.'}, status: 400
+      end 
+    end
+
+    def destroy
+      @user = User.find_by(id: params[:id])
+      if @user
+        @user.destroy
+        render json: {message: 'User deleted.'}
+      else
+        render json: {error: 'User not found.'}, status: 404
       end
+    end
+
+    def update
+      @user = User.find_by(id: params[:id])
+      if @user
+        @user.update(name: params[:name])
+        render  json: @user
+      else 
+        render json: {error: 'User not found.'}, status: 404
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :users
-resources :workouts
-resources :workout_exercises
+  resources :users
+  resources :workouts
+  resources :workout_exercises
+
+  post 'signin', to: 'users#signin'
+  get 'validate', to: 'users#validate'
+  get 'workouts', to: 'users#get_workout'
+
 end
